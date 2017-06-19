576a9167a80f892f14d7c7b0db8e0c8911e30b82
phchan9 <phckhc@gmail.com>
2017-06-19 23:24:09 +0800

    fixed some warning by react-script

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import App from './components/App';
-import reducers from './reducers';
 import store from './store/configureStore';
 
 ReactDOM.render(

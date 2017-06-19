aba467ee352a1961e5937884762e2e20bb173746
phchan9 <phckhc@gmail.com>
2017-06-19 22:34:38 +0800

    update App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import TopicListContainer from '../containers/TopicListContainer';
+import AddTopicContainer from '../containers/AddTopicContainer';
 
 const App = () => (
-  <div>
+  <div clasName="flex-column">
+    <AddTopicContainer />
+    <TopicListContainer />
   </div>
 );
 
